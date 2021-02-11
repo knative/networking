5a67e064011f7b7b131eb2c144e1427abeaddc53
Markus Thömmes <markusthoemmes@me.com>
2021-02-11 23:00:24 +0100

    Rename and touchups

--- pkg/network.go
+++ pkg/network.go
@@ -176,10 +176,10 @@ const (
 	// consistent.
 	VisibilityLabelKey = "networking.knative.dev/visibility"
 
-	// DirectLoadbalancingHeaderName is the name of a header that signifies to
-	// loadbalancers that the respective request should not be loadbalanced again but
-	// sent to the request's target directly.
-	DirectLoadbalancingHeaderName = "K-Direct-Lb"
+	// PassthroughLoadbalancingHeaderName is the name of the header that directs
+	// load balancers to not load balance the respective request but to
+	// send it to the request's target directly.
+	PassthroughLoadbalancingHeaderName = "K-Passthrough-Lb"
 )
 
 // DomainTemplateValues are the available properties people can choose from
