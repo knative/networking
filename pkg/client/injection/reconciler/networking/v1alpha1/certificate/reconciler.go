a64b4beb7ef79c067f8ba715310f5d3956fc0922
knative-automation <automation@knative.team>
2021-04-12 21:52:00 -0700

    upgrade to latest dependencies (#391)
    bumping knative.dev/pkg 3a2ae6d...b51994e:
      > b51994e Fix a few comments in reconciler-gen (# 2090)
    
    Signed-off-by: Knative Automation <automation@knative.team>

--- pkg/client/injection/reconciler/networking/v1alpha1/certificate/reconciler.go
+++ pkg/client/injection/reconciler/networking/v1alpha1/certificate/reconciler.go
@@ -88,13 +88,13 @@ type doReconcile func(ctx context.Context, o *v1alpha1.Certificate) reconciler.E
 
 // reconcilerImpl implements controller.Reconciler for v1alpha1.Certificate resources.
 type reconcilerImpl struct {
-	// LeaderAwareFuncs is inlined to help us implement reconciler.LeaderAware
+	// LeaderAwareFuncs is inlined to help us implement reconciler.LeaderAware.
 	reconciler.LeaderAwareFuncs
 
 	// Client is used to write back status updates.
 	Client versioned.Interface
 
-	// Listers index properties about resources
+	// Listers index properties about resources.
 	Lister networkingv1alpha1.CertificateLister
 
 	// Recorder is an event recorder for recording Event resources to the
@@ -119,7 +119,7 @@ type reconcilerImpl struct {
 	classValue string
 }
 
-// Check that our Reconciler implements controller.Reconciler
+// Check that our Reconciler implements controller.Reconciler.
 var _ controller.Reconciler = (*reconcilerImpl)(nil)
 
 // Check that our generated Reconciler is always LeaderAware.
--- pkg/client/injection/reconciler/networking/v1alpha1/domain/reconciler.go
+++ pkg/client/injection/reconciler/networking/v1alpha1/domain/reconciler.go
@@ -88,13 +88,13 @@ type doReconcile func(ctx context.Context, o *v1alpha1.Domain) reconciler.Event
 
 // reconcilerImpl implements controller.Reconciler for v1alpha1.Domain resources.
 type reconcilerImpl struct {
-	// LeaderAwareFuncs is inlined to help us implement reconciler.LeaderAware
+	// LeaderAwareFuncs is inlined to help us implement reconciler.LeaderAware.
 	reconciler.LeaderAwareFuncs
 
 	// Client is used to write back status updates.
 	Client versioned.Interface
 
-	// Listers index properties about resources
+	// Listers index properties about resources.
 	Lister networkingv1alpha1.DomainLister
 
 	// Recorder is an event recorder for recording Event resources to the
@@ -116,7 +116,7 @@ type reconcilerImpl struct {
 	skipStatusUpdates bool
 }
 
-// Check that our Reconciler implements controller.Reconciler
+// Check that our Reconciler implements controller.Reconciler.
 var _ controller.Reconciler = (*reconcilerImpl)(nil)
 
 // Check that our generated Reconciler is always LeaderAware.
