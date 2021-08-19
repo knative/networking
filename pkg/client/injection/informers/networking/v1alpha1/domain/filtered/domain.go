9b9af65da027ebf1b263c1ed419d61bb59d09278
Knative Automation <automation@knative.team>
2021-08-19 01:23:22 +0000

    upgrade to latest dependencies
    bumping knative.dev/hack e28525d...815cd31:
      > 815cd31 Stop performing pre-releases (# 77)
    bumping knative.dev/pkg 21eb4c1...7b5ecbc:
      > 7b5ecbc Sketch out an alternate way of injecting clients/informers (# 2210)
      > 8c88fa2 Revert "Remove the unused file (# 1935)" (# 2213)
      > 50410e0 Use consistent case for "Deprecated" comments (# 2216)
      > 35bcd16 Drop redundant pointers and decoders (# 2211)
      > bf176d5 Sink two codegen passes below loop. (# 2209)
      > 2c8a7b6 Update community files (# 2208)
      > 23b0147 Update actions (# 2206)
    
    Signed-off-by: Knative Automation <automation@knative.team>

--- pkg/client/injection/informers/networking/v1alpha1/domain/filtered/domain.go
+++ pkg/client/injection/informers/networking/v1alpha1/domain/filtered/domain.go
@@ -21,8 +21,15 @@ package filtered
 import (
 	context "context"
 
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	labels "k8s.io/apimachinery/pkg/labels"
+	cache "k8s.io/client-go/tools/cache"
+	apisnetworkingv1alpha1 "knative.dev/networking/pkg/apis/networking/v1alpha1"
+	versioned "knative.dev/networking/pkg/client/clientset/versioned"
 	v1alpha1 "knative.dev/networking/pkg/client/informers/externalversions/networking/v1alpha1"
+	client "knative.dev/networking/pkg/client/injection/client"
 	filtered "knative.dev/networking/pkg/client/injection/informers/factory/filtered"
+	networkingv1alpha1 "knative.dev/networking/pkg/client/listers/networking/v1alpha1"
 	controller "knative.dev/pkg/controller"
 	injection "knative.dev/pkg/injection"
 	logging "knative.dev/pkg/logging"
@@ -30,6 +37,7 @@ import (
 
 func init() {
 	injection.Default.RegisterFilteredInformers(withInformer)
+	injection.Dynamic.RegisterDynamicInformer(withDynamicInformer)
 }
 
 // Key is used for associating the Informer inside the context.Context.
@@ -54,6 +62,20 @@ func withInformer(ctx context.Context) (context.Context, []controller.Informer) 
 	return ctx, infs
 }
 
+func withDynamicInformer(ctx context.Context) context.Context {
+	untyped := ctx.Value(filtered.LabelKey{})
+	if untyped == nil {
+		logging.FromContext(ctx).Panic(
+			"Unable to fetch labelkey from context.")
+	}
+	labelSelectors := untyped.([]string)
+	for _, selector := range labelSelectors {
+		inf := &wrapper{client: client.Get(ctx), selector: selector}
+		ctx = context.WithValue(ctx, Key{Selector: selector}, inf)
+	}
+	return ctx
+}
+
 // Get extracts the typed informer from the context.
 func Get(ctx context.Context, selector string) v1alpha1.DomainInformer {
 	untyped := ctx.Value(Key{Selector: selector})
@@ -62,4 +84,47 @@ func Get(ctx context.Context, selector string) v1alpha1.DomainInformer {
 			"Unable to fetch knative.dev/networking/pkg/client/informers/externalversions/networking/v1alpha1.DomainInformer with selector %s from context.", selector)
 	}
 	return untyped.(v1alpha1.DomainInformer)
+}
+
+type wrapper struct {
+	client versioned.Interface
+
+	selector string
+}
+
+var _ v1alpha1.DomainInformer = (*wrapper)(nil)
+var _ networkingv1alpha1.DomainLister = (*wrapper)(nil)
+
+func (w *wrapper) Informer() cache.SharedIndexInformer {
+	return cache.NewSharedIndexInformer(nil, &apisnetworkingv1alpha1.Domain{}, 0, nil)
+}
+
+func (w *wrapper) Lister() networkingv1alpha1.DomainLister {
+	return w
+}
+
+func (w *wrapper) List(selector labels.Selector) (ret []*apisnetworkingv1alpha1.Domain, err error) {
+	reqs, err := labels.ParseToRequirements(w.selector)
+	if err != nil {
+		return nil, err
+	}
+	selector = selector.Add(reqs...)
+	lo, err := w.client.NetworkingV1alpha1().Domains().List(context.TODO(), v1.ListOptions{
+		LabelSelector: selector.String(),
+		// TODO(mattmoor): Incorporate resourceVersion bounds based on staleness criteria.
+	})
+	if err != nil {
+		return nil, err
+	}
+	for idx := range lo.Items {
+		ret = append(ret, &lo.Items[idx])
+	}
+	return ret, nil
+}
+
+func (w *wrapper) Get(name string) (*apisnetworkingv1alpha1.Domain, error) {
+	// TODO(mattmoor): Check that the fetched object matches the selector.
+	return w.client.NetworkingV1alpha1().Domains().Get(context.TODO(), name, v1.GetOptions{
+		// TODO(mattmoor): Incorporate resourceVersion bounds based on staleness criteria.
+	})
 }
